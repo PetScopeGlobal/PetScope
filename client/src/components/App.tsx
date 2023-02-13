0d971e24b698f0ff36648c11f33ba025932d3fdd
Scott Robinson <scott.robinson2@sky.uk>
2023-02-13 15:24:30 +0000

    Temp API calls

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,22 +1,51 @@
-import logo from "../assets/logo.svg";
+import { Client } from "@petfinder/petfinder-js";
+import { useEffect, useState } from "react";
 import "../styles/App.css";
 
+const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
+if (!(REACT_APP_CLIENT_ID && REACT_APP_CLIENT_SECRET)) {
+    throw new Error("Missing REACT_APP_CLIENT_ID and/or REACT_APP_CLIENT_SECRET env variable");
+}
+
+const client = new Client({
+    apiKey: REACT_APP_CLIENT_ID,
+    secret: REACT_APP_CLIENT_SECRET,
+});
+
 export default function App() {
+    const [animals, setAnimals] = useState([]);
+
+    useEffect(() => {
+        console.log("App useEffect triggered");
+
+        client.animalData
+            .types()
+            .then(types => {
+                console.log(types);
+                for (const type of types.data.types) {
+                    client.animalData.breeds(type.name).then(async breeds => {
+                        const typeResp = await client.animalData.type(type.name);
+                        console.log({ type, breeds: breeds.data, typeResp: typeResp.data });
+                    });
+                }
+            })
+            .catch(console.error);
+
+        client.animal
+            .search()
+            .then(resp => {
+                console.log(resp.data);
+                setAnimals(resp.data.animals);
+            })
+            .catch(console.error);
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/components/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {animals.map((animal: Record<"id", number>) => {
+                    return <p key={animal.id}>{animal.id}</p>;
+                })}
             </header>
         </div>
     );
